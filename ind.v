def1a9d650770c5aea1abd90a4b55312424518ff
Hexirp <Hexirp@users.noreply.github.com>
2017-08-17 08:22:02 +0900

    Add composing

--- ind.v
+++ ind.v
@@ -1,48 +1,27 @@
 Require Import Init Basics.
 
-Extraction Language Haskell.
+Inductive composing (k : Type -> Type -> Type) (a b : Type) : Type :=
+ | leaf : k a b -> composing k a b
+ | tree : forall x, composing k a x -> composing k x b -> composing k a b.
 
-Inductive exist (p : Type -> Type) : Type :=
- | exist_mk : forall x, p x -> exist p.
-
-Definition run_exist p r : (forall x, p x -> r) -> exist p -> r.
+Definition compose
+ : forall k a b, (forall x y z, k x y -> k y z -> k x z) -> composing k a b -> k a b.
 Proof.
- intros f.
- intros x.
- destruct x as [X x].
- apply (f X).
- apply x.
-Defined.
-
-Print run_exist.
-
-Inductive cat (k : Type -> Type -> Type) (a b : Type) : Type :=
- | leaf : k a b -> cat k a b
- | tree : (exist (fun x => cat k a x * cat k x b)%type) -> cat k a b.
-
-Definition run_cat k a b r : (k a b -> r) -> ((exist (fun x => cat k a x * cat k x b)%type) -> r) -> cat k a b -> r.
-Proof.
- intros emp rec.
- intros x.
- destruct x as [lea | tre].
+ fix 5.
+ intros k a b f x.
+ case x.
  -
-  apply emp.
-  apply lea.
+  intros xLeaf.
+  apply xLeaf.
  -
-  apply rec.
-  apply tre.
-Save.
-
-Definition run_cat' k a b r : (k a b -> r) -> (forall x, (cat k a x * cat k x b)%type -> r) -> cat k a b -> r.
-Proof.
- intros emp rec.
- intros x.
- apply (run_cat k a b r).
- -
-  apply emp.
- -
-  apply run_exist.
-  apply rec.
- -
-  apply x.
+  intros xImpl xLeft xRight.
+  apply f with xImpl.
+  +
+   apply compose.
+   apply f.
+   apply xLeft.
+  +
+   apply compose.
+   apply f.
+   apply xRight.
 Save.
