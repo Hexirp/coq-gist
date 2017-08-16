158fa0064cdd2982bb192c0a49d956e615a72fc8
Hexirp <Hexirp@users.noreply.github.com>
2017-08-17 08:45:15 +0900

    Add viewL

--- ind.v
+++ ind.v
@@ -7,7 +7,7 @@ Inductive composing (k : Type -> Type -> Type) (a b : Type) : Type :=
 Definition compose
  : forall k a b, (forall x y z, k x y -> k y z -> k x z) -> composing k a b -> k a b.
 Proof.
- fix 5.
+ fix go 5.
  intros k a b f x.
  case x.
  -
@@ -17,11 +17,45 @@ Proof.
   intros xImpl xLeft xRight.
   apply f with xImpl.
   +
-   apply compose.
+   apply go.
    apply f.
    apply xLeft.
   +
-   apply compose.
+   apply go.
    apply f.
    apply xRight.
+Save.
+
+Definition viewL
+ : forall k a b r,
+  composing k a b -> (k a b -> r) -> (forall x, k a x ->  composing k x b -> r) -> r.
+Proof.
+ intros k a b r x fL fT.
+ case x.
+ -
+  intros xLeft.
+  apply fL.
+  apply xLeft.
+ -
+  fix go 2.
+  intros xImpl xLeft xRight.
+  case xLeft.
+  +
+   intros xLeftLeaf.
+   apply fT with xImpl.
+   *
+    apply xLeftLeaf.
+   *
+    apply xRight.
+  +
+   intros xLeftImpl xLeftLeft xLeftRight.
+   apply go with xLeftImpl.
+   *
+    apply xLeftLeft.
+   *
+    apply tree with xImpl.
+    --
+     apply xLeftRight.
+    --
+     apply xRight.
 Save.
