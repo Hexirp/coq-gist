2e8c135b56a752d185a4289527e0a691b2b48b82
Hexirp <Hexirp@users.noreply.github.com>
2017-08-28 21:14:46 +0900

    Update

--- object.v
+++ object.v
@@ -3,5 +3,38 @@ Require Import Init.
 Inductive coyoneda (f : Type -> Type) (a : Type) : Type :=
  | mkcoyoneda : forall x, (x -> a) -> f x -> coyoneda f a.
 
+Definition lift f a : f a -> coyoneda f a.
+Proof.
+ intros af.
+ apply mkcoyoneda with a.
+ -
+  intros A.
+  apply A.
+ -
+  apply af.
+Save.
+
+Definition map f a b : (a -> b) -> coyoneda f a -> coyoneda f b.
+Proof.
+ intros ba afCo.
+ case afCo.
+ intros x ax xf.
+ apply mkcoyoneda with x.
+ -
+  intros X.
+  apply ba.
+  apply ax.
+  apply X.
+ -
+  apply xf.
+Save.
+
 Inductive object (f g : Type -> Type) : Type :=
- | mkobj : (forall x, f x -> coyoneda g (prod x (object f g))) -> object f g.
+ | mkobj : (forall x, coyoneda f x -> coyoneda g (prod x (object f g))) -> object f g.
+
+Definition compose f g h : object g h -> object f g -> object f h.
+Proof.
+ intros hgObj gfObj.
+ apply mkobj.
+ intros x xfCo.
+ apply 
