90b175f074cdf662a494782b1cab3a4b0c07c760
Hexirp <Hexirp@users.noreply.github.com>
2017-08-28 20:57:15 +0900

    Update

--- object.v
+++ object.v
@@ -1,18 +1,48 @@
 Require Import Init.
 
-Inductive coyoneda (f : Type -> Type) (a : Type) : Type :=
- cy : forall x, f x -> (x -> a) -> coyoneda f a.
+Axiom object_wrong : (Type -> Type) -> (Type -> Type) -> Type.
+Axiom mkobj_wrong : forall f g,
+ (forall x, f x -> g (prod x (object_wrong f g))) -> object_wrong f g.
+Axiom object_wrong_rect : forall f g (P : object_wrong f g -> Type),
+ (forall p, P (mkobj_wrong f g p)) -> forall o, P o.
+Axiom object_wrong_ind : forall f g (P : object_wrong f g -> Prop),
+ (forall p, P (mkobj_wrong f g p)) -> forall o, P o.
+Axiom object_wrong_rec : forall f g (P : object_wrong f g -> Set),
+ (forall p, P (mkobj_wrong f g p)) -> forall o, P o.
 
-Definition map_c : forall f a a', (a -> a') -> coyoneda f a -> coyoneda f a'.
+Definition run_object_wrong (f g : Type -> Type)
+ : object_wrong f g -> forall x, f x -> g (prod x (object_wrong f g)).
 Proof.
- intros f a a' F A.
- induction A.
- apply cy with x.
+ intros o x xf.
+ revert o.
+ apply object_wrong_rect.
+ intros p.
+ apply p.
+ apply xf.
+Save.
+
+Definition wrong (f g : Type -> Type) (x : Type)
+ : (forall a, g a -> a -> x) -> x -> f x -> object_wrong f g -> x.
+Proof.
+ intros get xv xf o.
+ apply get with (prod x (object_wrong f g)).
  -
-  apply f0.
+  apply (run_object_wrong f g).
+  +
+   apply o.
+  +
+   apply xf.
  -
-  intros xv.
-  apply F.
-  apply a0.
-  apply xv.
-Defined.
+  split.
+  +
+   apply xv.
+  +
+   apply o.
+Save.
+
+Print wrong.
+
+Inductive object (f g : Type -> Type) : Type :=
+ | mkobj : (forall x, f x -> prod x (object f g)) -> object f g.
+
+Check object_rect.
