a5ffc340dffefe8eea0126b8de2c6711c4b4931a
Hexirp <Hexirp@users.noreply.github.com>
2017-09-06 20:36:41 +0900

    Update object.v

--- object.v
+++ object.v
@@ -20,3 +20,36 @@ Definition Ω (x : wrong) : nat :=
 *)
 Fail Inductive wrong : Type :=
 | wrong_C : (wrong -> nat) -> wrong.
+
+(** 上記の減少を防ぐ方法を提案する。
+
+上記の型が帰納的に使われている位置を見てみると、これはcontravariantな位置である。
+つまり、functorな位置であればこの問題は発生しないことになる。
+つまり、objectの場合ではGがfunctorしか受け取れないようにすればよい。
+coyonedaを使ってたとえcontravariantな型が渡されても無理やりfunctorに変えればよい。
+
+*)
+Inductive coyoneda (F : Type -> Type) (A : Type) : Type :=
+| coyoneda_C : forall X, (X -> A) -> F X -> coyoneda F A.
+
+Definition coyoneda_map F A B : (A -> B) -> coyoneda F A -> coyoneda F B.
+Proof.
+ intros f [X g x].
+ apply coyoneda_C with X; auto.
+Defined.
+
+(** coyonedaを使ってもまだ問題があることを示す。
+
+重複した名前を使いたいため、Moduleを使う。
+
+*)
+Module WObject.
+ (** coyonedaによる問題の解決 *)
+ Inductive object (F G : Type -> Type) : Type :=
+ | object_C : (forall A, F A -> coyoneda G (A * object F G)) -> object F G.
+
+ Definition object_run F G A : object F G -> F A -> coyoneda G (A * object F G).
+ Proof.
+  intros [f].
+  apply f.
+ Defined.
