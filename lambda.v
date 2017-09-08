c49a269e0db9f66f89e00af0615a443a09a78b51
Hexirp <Hexirp@users.noreply.github.com>
2017-09-08 22:58:33 +0900

    Add lambda.v

---
+++ lambda.v
@@ -0,0 +1,10 @@
+Inductive fin : nat -> Type :=
+| fin_O : forall n, fin n
+| fin_S : forall n, fin n -> fin (S n).
+
+Inductive lambda : nat -> Type :=
+| var : forall n, fin n -> lambda n
+| abs : forall n, lambda (S n) -> lambda n
+| app : forall n, lambda n -> lambda n -> lambda n.
+
+Definition closed := lambda 0.
