aec0cd0732f071b0d884ccef98b58bfa29613e69
Hexirp <Hexirp@users.noreply.github.com>
2017-09-09 18:19:51 +0900

    Fix lambda

--- lambda.v
+++ lambda.v
@@ -3,7 +3,7 @@ Inductive fin : nat -> Type :=
 | fin_S : forall n, fin n -> fin (S n).
 
 Inductive lambda : nat -> Type :=
-| var : forall n, fin n -> lambda n
+| var : forall n, fin n -> lambda (S n)
 | abs : forall n, lambda (S n) -> lambda n
 | app : forall n, lambda n -> lambda n -> lambda n.
 
