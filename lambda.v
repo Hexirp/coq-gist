bab94befb4d369ad370aa88c11625ca0c08b2327
Hexirp <Hexirp@users.noreply.github.com>
2017-09-09 21:54:44 +0900

    Add lambda_bet_lemma_eq

--- lambda.v
+++ lambda.v
@@ -9,10 +9,20 @@ Inductive lambda : nat -> Type :=
 
 Definition closed := lambda 0.
 
+Definition lambda_bet_lemma_eq
+ : forall (P: nat -> Prop) (n : nat), (forall (m : nat), m = S n -> P m) -> P (S n).
+Proof.
+ intros P n h.
+ apply h.
+ apply f_equal.
+ apply eq_refl.
+Qed.
+
 Definition lambda_bet : forall n, lambda (S n) -> lambda n -> lambda n.
 Proof.
  intros n x y.
  inversion x.
+ Abort.
 
 Definition lambda_app : forall n, lambda n -> lambda n -> lambda n.
 Proof.
