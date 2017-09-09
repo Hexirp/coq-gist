f049325fe9abb98dac1771a75801b6ccf792ace0
Hexirp <Hexirp@users.noreply.github.com>
2017-09-09 19:10:33 +0900

    Update

--- lambda.v
+++ lambda.v
@@ -9,6 +9,35 @@ Inductive lambda : nat -> Type :=
 
 Definition closed := lambda 0.
 
+Definition lambda_bet : forall n, lambda n -> lambda (S n).
+Proof.
+ apply (lambda_rect (fun n _ => lambda (S n))).
+ -
+  intros n x_var.
+  apply var.
+  revert n x_var.
+  apply (fin_rect (fun n _ => fin (S n))).
+  +
+   intros n.
+   apply fin_S.
+   apply fin_O.
+  +
+   intros n _ x_var.
+   apply fin_S.
+   apply x_var.
+ -
+  intros n _ x_abs.
+  apply abs.
+  apply x_abs.
+ -
+  intros n _ x_app_1 _ x_app_2.
+  apply app.
+  +
+   apply x_app_1.
+  +
+   apply x_app_2.
+Defined.
+
 Definition lambda_app : forall n, lambda n -> lambda n -> lambda n.
 Proof.
  apply (lambda_rect (fun n _ => lambda n -> lambda n)).
@@ -21,5 +50,5 @@ Proof.
   +
    apply y.
  -
-  intros n x_abs y_abs y.
+  intros n _ y_abs y.
   Abort.
