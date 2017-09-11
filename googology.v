52c7d1ff3319751d60fe083db142f2688db2c56d
Hexirp <Hexirp@users.noreply.github.com>
2017-09-11 19:05:09 +0900

    Update

--- googology.v
+++ googology.v
@@ -3,37 +3,113 @@ Require Import Init.
 (** 全ての関数はこのような型を取る *)
 Definition from (a : Type) := a -> nat.
 
+(** シンプルな帰納法 *)
+Definition nat_rect_simple : forall (R : Type), R -> (R -> R) -> nat -> R.
+Proof.
+ intros R case_O case_S n.
+ apply (nat_rect (fun _ => R)).
+ -
+  apply case_O.
+ -
+  intros _.
+  apply case_S.
+ -
+  apply n.
+Defined.
+
 (** おそらく最小の関数 *)
-Definition null : from nat := fun _ => O.
+Definition null : from nat.
+Proof.
+ intros _.
+ apply O.
+Defined.
 
 (** nullを一般化する。 *)
-Definition const (n : nat) : from nat := fun _ => n.
+Definition const (n : nat) : from nat.
+Proof.
+ intros _.
+ apply n.
+Defined.
 
 (** 恒等関数 *)
-Definition idfunc : from nat := fun x => x.
+Definition id : from nat.
+Proof.
+ intros n.
+ apply const.
+ -
+  apply n.
+ -
+  apply n.
+Defined.
 
 (** 後者関数 *)
-Definition succ : from nat := fun x => S x.
+Definition succ : from nat.
+Proof.
+ intros n.
+ apply const.
+ -
+  apply S.
+  apply n.
+ -
+  apply n.
+Defined.
 
 (** 加算 *)
-Fixpoint plus (n : nat) : from nat :=
- match n with
- | O => idfunc
- | S n' => fun n => succ (plus n' n)
- end.
+Definition plus (n : nat) : from nat.
+Proof.
+ revert n.
+ apply nat_rect_simple.
+ -
+  apply id.
+ -
+  intros f n.
+  apply S.
+  apply f.
+  apply n.
+Defined.
 
 (** 二倍する *)
-Definition dual : from nat := fun n => plus n n.
+Definition dual : from nat.
+Proof.
+ intros n.
+ apply plus.
+ -
+  apply n.
+ -
+  apply n.
+Defined.
 
 (** 三倍する *)
-Definition trip : from nat := fun n => plus n (plus n n).
+Definition trip : from nat.
+Proof.
+ intros n.
+ apply plus.
+ -
+  apply n.
+ -
+  apply plus.
+  +
+   apply n.
+  +
+   apply n.
+Defined.
 
 (** 乗算 *)
-Fixpoint mult (n : nat) : from nat :=
- match n with
- | O => null
- | S n' => fun n => plus n (mult n' n)
- end.
+Definition mult (n : nat) : from nat.
+Proof.
+ revert n.
+ apply nat_rect_simple.
+ -
+  apply null.
+ -
+  intros f n.
+  apply plus.
+  +
+   apply n.
+  +
+   apply f.
+   apply n.
+Defined.
 
 (** ハイパー演算子 *)
 Fixpoint hype (n : nat) : from (from nat) :=
