93517f1b000f7c99e755773de25c0b48ef545924
Hexirp <Hexirp@users.noreply.github.com>
2017-09-22 22:07:22 +0900

    Abort

--- lambda.v
+++ lambda.v
@@ -7,10 +7,27 @@ Inductive lambda : nat -> Type :=
 | abs : forall n, lambda (S n) -> lambda n
 | app : forall n, lambda n -> lambda n -> lambda n.
 
-Definition lambda_bet : forall n, lambda (S n) -> lambda n -> lambda n.
+Definition lambda_b_s_case_var : forall n, fin n -> lambda n -> lambda n.
 Proof.
- intros n.
- apply (lambda_bet_lemma_eq (fun n' => lambda n' -> lambda n -> lambda n)).
+ intros n f y.
+ induction f.
+ -
+  apply y.
+ -
+  apply var.
+  apply f.
+Defined.
+
+Definition lambda_beta_simplfy : forall n, lambda (S n) -> lambda n -> lambda n.
+Proof.
+ intros n x y.
+ induction x.
+ -
+  apply lambda_b_s_case_var.
+ -
+  induction y.
+  +
+   
  intros m h x y.
  revert m x h y.
  apply (lambda_rect (fun o _ => o = S n -> lambda n -> lambda n)).
