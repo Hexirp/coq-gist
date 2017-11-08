1aa2710c3a5c7fc32b8b08362a0637d6275ff55d
Hexirp <Hexirp@users.noreply.github.com>
2017-11-08 21:46:49 +0900

    Add new_p

--- set.v
+++ set.v
@@ -26,11 +26,15 @@ Fixpoint uniques (a : list nat) : bool :=
 
 Definition set := { x : list nat | eq_true (uniques x) }.
 
+Definition new_p (n : nat) (s : list nat) (p : eq_true (uniques s)) (q : eq_true (inl n s))
+    : eq_true (uniques (cons n s)) :=
+ undefined (eq_true (uniques (cons n s))).
+
 Definition add (n : nat) (s : set) : set :=
  match s with
  | exist _ s' p =>
   match inl n s' with
-  | false => exist _ s' (undefined (eq_true (uniques s')))
+  | false => exist _ (cons n s') (new_p n s' p (undefined (eq_true (inl n s'))))
   | true => exist _ s' p
   end
  end
