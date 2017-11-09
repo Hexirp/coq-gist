069842d508341d87c1916fb39e39d0a654214850
Hexirp <Hexirp@users.noreply.github.com>
2017-11-09 21:56:27 +0900

    Add extraction to Haskell

--- set.v
+++ set.v
@@ -1,56 +1,62 @@
-Require Import Init Nat.
+Module sets.
+ Require Import Init Nat.
 
-Fixpoint ninl (n : nat) (a : list nat) : bool :=
- match a with
- | nil => true
- | cons x a' =>
-  match eqb n x with
-  | false => ninl n a'
-  | true => false
+ Fixpoint ninl (n : nat) (a : list nat) : bool :=
+  match a with
+  | nil => true
+  | cons x a' =>
+   match eqb n x with
+   | false => ninl n a'
+   | true => false
+   end
   end
- end
-.
+ .
 
-Fixpoint uniques (a : list nat) : bool :=
- match a with
- | nil => true
- | cons x a' =>
-  match ninl x a' with
-  | false => false
-  | true => uniques a'
+ Fixpoint uniques (a : list nat) : bool :=
+  match a with
+  | nil => true
+  | cons x a' =>
+   match ninl x a' with
+   | false => false
+   | true => uniques a'
+   end
   end
- end
-.
+ .
 
-Definition set := { x : list nat | eq_true (uniques x) }.
+ Definition set := { x : list nat | eq_true (uniques x) }.
 
-Definition add_uniques (n : nat) (s : list nat) (p : eq_true (uniques s)) (q : eq_true (ninl n s))
-    : eq_true (uniques (cons n s)).
-Proof.
- unfold uniques.
- fold (uniques s).
- destruct (ninl n s).
- -
-  apply p.
- -
-  apply q.
-Defined.
+ Definition add_uniques (n : nat) (s : list nat) (p : eq_true (uniques s)) (q : eq_true (ninl n s))
+     : eq_true (uniques (cons n s)).
+ Proof.
+  unfold uniques.
+  fold (uniques s).
+  destruct (ninl n s).
+  -
+   apply p.
+  -
+   apply q.
+ Defined.
 
-Definition if_eq_true {r : Type} (b : bool) (t : eq_true b -> r) (f : r) : r.
-Proof.
- destruct b.
- -
-  apply t.
-  apply is_eq_true.
- -
-  apply f.
-Defined.
+ Definition if_eq_true {r : Type} (b : bool) (t : eq_true b -> r) (f : r) : r.
+ Proof.
+  destruct b.
+  -
+   apply t.
+   apply is_eq_true.
+  -
+   apply f.
+ Defined.
 
-Definition add (n : nat) (s : set) : set :=
- match s with
- | exist _ s' p =>
-  if_eq_true (ninl n s')
-   (fun q => exist _ (cons n s') (add_uniques n s' p q))
-   (exist _ s' p)
- end
-.
+ Definition add (n : nat) (s : set) : set :=
+  match s with
+  | exist _ s' p =>
+   if_eq_true (ninl n s')
+    (fun q => exist _ (cons n s') (add_uniques n s' p q))
+    (exist _ s' p)
+  end
+ .
+End sets.
+
+Extraction Language Haskell.
+
+Extraction sets.
