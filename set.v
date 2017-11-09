a7affa4b1f7f4a04234291e6cbe3be5bc94a4960
Hexirp <Hexirp@users.noreply.github.com>
2017-11-09 20:54:30 +0900

    Update inl -> ninl

--- set.v
+++ set.v
@@ -2,13 +2,13 @@ Require Import Init Nat.
 
 Axiom undefined : forall a, a.
 
-Fixpoint inl (n : nat) (a : list nat) : bool :=
+Fixpoint ninl (n : nat) (a : list nat) : bool :=
  match a with
  | nil => true
  | cons x a' =>
   match eqb n x with
-  | false => inl n a'
-  | true => true
+  | false => ninl n a'
+  | true => false
   end
  end
 .
@@ -17,24 +17,24 @@ Fixpoint uniques (a : list nat) : bool :=
  match a with
  | nil => true
  | cons x a' =>
-  match inl x a' with
-  | false => uniques a'
-  | true => false
+  match ninl x a' with
+  | false => false
+  | true => uniques a'
   end
  end
 .
 
 Definition set := { x : list nat | eq_true (uniques x) }.
 
-Definition new_p (n : nat) (s : list nat) (p : eq_true (uniques s)) (q : eq_true (inl n s))
+Definition new_p (n : nat) (s : list nat) (p : eq_true (uniques s)) (q : eq_true (ninl n s))
     : eq_true (uniques (cons n s)) :=
  undefined (eq_true (uniques (cons n s))).
 
 Definition add (n : nat) (s : set) : set :=
  match s with
  | exist _ s' p =>
-  match inl n s' with
-  | false => exist _ (cons n s') (new_p n s' p (undefined (eq_true (inl n s'))))
+  match ninl n s' with
+  | false => exist _ (cons n s') (new_p n s' p (undefined (eq_true (ninl n s'))))
   | true => exist _ s' p
   end
  end
