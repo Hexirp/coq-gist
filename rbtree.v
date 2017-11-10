6ffd159cd40221314977235dcde09df6b7882497
Hexirp <Hexirp@users.noreply.github.com>
2017-11-10 16:07:33 +0900

    Add tree_func

--- rbtree.v
+++ rbtree.v
@@ -6,4 +6,14 @@ Module rbtree.
  | bt : nat -> tree -> tree -> tree
  | rt : nat -> tree -> tree -> tree
  .
+
+ Definition tree_func {r : Type} (nr : r) (br : nat -> r -> r -> r) (rr : nat -> r -> r -> r)
+     : tree -> r :=
+ fix go (t : tree) : r :=
+  match t with
+  | ni => nr
+  | bt n tl tr => br n (go tl) (go tr)
+  | rt n tl tr => rr n (go tl) (go tr)
+  end
+ .
 End rbtree.
