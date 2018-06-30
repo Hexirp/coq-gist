d065238085adc3729a4e50f55469ee18666f5f0e
Hexirp <Hexirp@users.noreply.github.com>
2018-06-30 18:36:03 +0900

    update

--- googology.v
+++ googology.v
@@ -2,6 +2,12 @@ Require Import Coq.Init.Prelude.
 
 Definition f0 := O.
 
-Definition f1 := S O.
+Definition f00 := S O.
 
-Definition f2 := S (S 0).
+Definition f00 := S (S 0).
+
+Definition f01 := fix f01 n := match n with O => O | S np => S np.
+
+Definition f010 := fun n => S (f01 n).
+
+Definition f0100 := fun n => S (f010 n).
