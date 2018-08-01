1e3053f6f1f666396045ab52c434b09254964452
Hexirp <Hexirp@users.noreply.github.com>
2018-08-01 15:03:49 +0900

    update

--- list.v
+++ list.v
@@ -8,9 +8,22 @@ Definition pointwise_eq {a : Type} {p : a -> Type} (f g : forall x : a, p x) : P
 
 Notation "f == g" := (pointwise_eq f g) (at level 70, no associativity) : type_scope.
 
+Definition id {A : Type} (x : A) : A := x.
+
 Definition compose {A B C : Type} (f : B -> C) (g : A -> B) (x : A) : C := f (g x).
 
 Notation "f 'o' g" := (compose f g) (at level 40, left associativity) : type_scope.
 
 Fixpoint fmap {a b : Type} (f : a -> b) (x : List a) : List b :=
  match x with Nil => Nil | Cons x xs => Cons (f x) (fmap f xs) end.
+
+Module Type Functor.
+ Parameter f : Type -> Type.
+ Parameter fmap : forall a b, (a -> b) -> f a -> f b.
+ Arguments fmap {a b} f x.
+
+ Axiom functor_law_1 : forall a, @id (f a) == fmap (@id a).
+ Axiom functor_law_2 : forall a b c, forall (f_ : b -> c) (g_ : a -> b),
+  fmap f_ o fmap g_ == fmap (f_ o g_).
+End Functor.
+
