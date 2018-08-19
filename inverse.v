c28d8c71d13027bf7dad699052027fc496107c7a
Hexirp <Hexirp@users.noreply.github.com>
2018-08-19 10:00:28 +0900

    Update: 日本語での解説を付ける
    より Wikipedia の定義の仕方に近くなるように inv_rel を削除

--- inverse.v
+++ inverse.v
@@ -1,39 +1,51 @@
 Require Import Coq.Init.Prelude.
 
+(** 点ごとに等しい *)
 Definition pointwise_eq {A B : Type} (f g : A -> B) : Prop := forall x, f x = g x.
 
 Notation "f == g" := (pointwise_eq f g) (at level 70, no associativity) : type_scope.
 
+(** 恒等射 *)
 Definition id {A : Type} : A -> A := fun x => x.
 
+(** 射の合成 *)
 Definition compose {A B C : Type} (f : B -> C) (g : A -> B) : A -> C := fun x => f (g x).
 
 Notation "g 'o' f" := (compose g%function f%function) (at level 40, left associativity)
  : function_scope.
 
-Definition inv_rel {A B : Type} (f : A -> B) (g : B -> A) : Prop := g o f == id.
 
+(** [f] は単射である *)
 Definition mono {A B : Type} (f : A -> B) : Prop
  := forall (Z : Type) (g₁ g₂ : Z -> A), f o g₁ == f o g₂ -> g₁ == g₂.
 
-Definition left_inv_rel {A B : Type} (f : A -> B) (g : B -> A) : Prop := inv_rel f g.
+(** [f] に対して [g] は左逆射である *)
+Definition left_inv_rel {A B : Type} (f : A -> B) (g : B -> A) : Prop := g o f == id.
 
+(** [f] は左逆射を持つ *)
 Definition left_inv {A B : Type} (f : A -> B) : Prop := ex (left_inv_rel f).
 
+(** [h] は分裂単射である *)
 Definition split_mono {A B : Type} (h : A -> B) : Prop := left_inv h /\ mono h.
 
+(** [f] は全射である *)
 Definition epi {A B : Type} (f : A -> B) : Prop
  := forall (C : Type) (g₁ g₂ : B -> C), g₁ o f == g₂ o f -> g₁ == g₂.
 
-Definition right_inv_rel {A B : Type} (f : A -> B) (g : B -> A) : Prop := inv_rel g f.
+(** [f] に対して [g] は右逆射である *)
+Definition right_inv_rel {A B : Type} (f : A -> B) (g : B -> A) : Prop := f o g == id.
 
+(** [f] は右逆射を持つ *)
 Definition right_inv {A B : Type} (f : A -> B) : Prop := ex (right_inv_rel f).
 
+(** [f] は分裂全射である *)
 Definition split_epi {A B : Type} (h : A -> B) : Prop := right_inv h /\ epi h.
 
+(** [f] は全単射／双射である *)
 Definition bi {A B : Type} (f : A -> B) : Prop := mono f /\ epi f.
 
-Definition iso_rel {A B : Type} (f : A -> B) (g : B -> A) : Prop := inv_rel f g /\ inv_rel g f.
+(** [f] と [g] は逆射の関係である *)
+Definition iso_rel {A B : Type} (f : A -> B) (g : B -> A) : Prop := g o f == id /\ f o g == id.
 
+(** [f] は同型射である *)
 Definition iso {A B : Type} (f : A -> B) : Prop := ex (iso_rel f).
-
