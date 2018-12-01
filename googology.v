b79cc5d50509903463e58942229a355158c53549
Hexirp <Hexirp@users.noreply.github.com>
2018-12-01 17:08:50 +0900

    Add: Succ の FGH

--- googology.v
+++ googology.v
@@ -3,4 +3,6 @@ Require Import Coq.Init.Prelude.
 
 Class FGH (A : Type) := { fgh : nat -> nat } .
 
-Instance FGH_Empty_set : FGH Empty_set := { fgh := S } .
+Instance FGH_Empty : FGH Empty_set := { fgh := S } .
+
+Instance FGH_Succ {A : Type} : FGH (sum unit A) := { fgh := S } .
