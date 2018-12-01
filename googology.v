d02c614c4b81b513e5e4788255f3f3aa8d33ac6e
Hexirp <Hexirp@users.noreply.github.com>
2018-12-01 17:11:22 +0900

    Fix: 正しい定義

--- googology.v
+++ googology.v
@@ -5,4 +5,13 @@ Class FGH (A : Type) := { fgh : nat -> nat } .
 
 Instance FGH_Empty : FGH Empty_set := { fgh := S } .
 
-Instance FGH_Succ {A : Type} : FGH (sum unit A) := { fgh := S } .
+Fixpoint iter {P : Type} (o : P) (s : P -> P) (n : nat) :=
+  match n with
+  | O => o
+  | S np => s (iter o s np)
+  end
+.
+
+Instance FGH_Succ {A : Type} : FGH (sum unit A) := {
+  fgh := fun n => iter n (fgh (A := A)) n
+} .
