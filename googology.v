10c08ef5abd289c0c6a9efbb883acc4379ccad6c
Hexirp <Hexirp@users.noreply.github.com>
2018-12-02 15:17:48 +0900

    Update: omega_m_omega_ まで

--- googology.v
+++ googology.v
@@ -16,7 +16,7 @@ Instance FGH_Succ (A : Type) `{FGH A} : FGH (sum unit A) := {
   fgh := fun n => iter n (fgh (A := A)) n
 } .
 
-Class FGH_forall {B : nat -> Type} :=
+Class FGH_forall (B : nat -> Type) :=
   fgh_forall : forall n, FGH (B n) .
 
 Instance FGH_sum (B : nat -> Type) `{FGH_forall B} : FGH (sigT B) := {
@@ -24,20 +24,87 @@ Instance FGH_sum (B : nat -> Type) `{FGH_forall B} : FGH (sigT B) := {
 } .
 
 
-(* fgh {0} n := n + 1 *)
+(* fgh {0} n = n + 1 *)
 Eval compute in fgh (A := Empty_set) 2 . (* 3 *)
 Eval compute in fgh (A := Empty_set) 3 . (* 4 *)
 Eval compute in fgh (A := Empty_set) 4 . (* 5 *)
 
-(* fgh {1} n := 2 * n *)
+(* fgh {1} n = 2 * n *)
 Eval compute in fgh (A := sum unit Empty_set) 2 . (* 4 *)
 Eval compute in fgh (A := sum unit Empty_set) 3 . (* 6 *)
 Eval compute in fgh (A := sum unit Empty_set) 4 . (* 8 *)
 
-(* fgh {2} n := (2 ^ n) * n *)
+(* fgh {2} n = (2 ^ n) * n *)
 Eval compute in fgh (A := sum unit (sum unit Empty_set)) 2 . (* 8 *)
 Eval compute in fgh (A := sum unit (sum unit Empty_set)) 3 . (* 24 *)
 Eval compute in fgh (A := sum unit (sum unit Empty_set)) 4 . (* 64 *)
 Eval compute in fgh (A := sum unit (sum unit Empty_set)) 5 . (* 160 *)
 Eval compute in fgh (A := sum unit (sum unit Empty_set)) 6 . (* 384 *)
 Eval compute in fgh (A := sum unit (sum unit Empty_set)) 7 . (* 896 *)
+
+(* fgh {3} n := ??? > 2 ^^ n *)
+Eval compute in fgh (A := sum unit (sum unit (sum unit Empty_set))) 2 . (* 2048 *)
+(* Eval compute in fgh (A := sum unit (sum unit (sum unit Empty_set))) 3 . *) (* 計算しようとすると落ちる *)
+
+
+(* ここまでの極限を求める *)
+
+Definition omega_ : nat -> Type := iter Empty_set (sum unit) .
+
+Instance FGH_forall_omega_ : FGH_forall omega_ .
+Proof.
+ intros n.
+ induction n.
+ -
+  exact _.
+ -
+  exact _.
+Defined.
+
+Definition omega : Type := sigT omega_ .
+
+Instance FGH_omega : FGH omega := _ .
+
+(* fgh {ω} n > Ack(n, n) *)
+Eval compute in fgh (A := omega) 1 . (* 1 *)
+Eval compute in fgh (A := omega) 2 . (* 8 *)
+(* Eval compute in fgh (A := omega) 3 . *) (* 計算しようとすると落ちる *)
+
+
+Definition omega_p_omega_ : nat -> Type := iter omega (sum unit) .
+
+Instance FGH_forall_omega_p_omega_ : FGH_forall omega_p_omega_ .
+Proof.
+ intros n.
+ induction n.
+ -
+  exact _.
+ -
+  exact _.
+Defined.
+
+Definition omega_p_omega : Type := sigT omega_p_omega_ .
+
+(* fgh {ω+ω} ≈ 爆発 (http://ja.googology.wikia.com/wiki/%E7%88%86%E7%99%BA) *)
+Eval compute in fgh (A := omega_p_omega) 1 . (* 2 *)
+(* Eval compute in fgh (A := omega_p_omega) 2 . *) (* 計算しようとすると落ちる *)
+
+
+(* さらに極限を取る。 *)
+
+(* Empty_set, (forall n, iter Empty_set (sum unit) n), (forall n, iter (forall n, iter Empty_set (sum unit) n) (sum unit) n), ... *)
+
+Definition omega_m_omega_ : nat -> Type .
+Proof.
+ apply iter.
+ -
+  exact Empty_set .
+ -
+  intro A.
+  refine (forall n : nat, _ n) .
+  apply iter.
+  +
+   exact A.
+  +
+   exact (sum unit).
+Defined.
