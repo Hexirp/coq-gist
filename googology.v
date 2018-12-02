fa8506a5a3cedb3b63cea5903b24ecdbf75d8c7f
Hexirp <Hexirp@users.noreply.github.com>
2018-12-02 14:35:56 +0900

    Update: fgh_sum

--- googology.v
+++ googology.v
@@ -12,6 +12,13 @@ Fixpoint iter {P : Type} (o : P) (s : P -> P) (n : nat) :=
   end
 .
 
-Instance FGH_Succ {A : Type} `{FGH A} : FGH (sum unit A) := {
+Instance FGH_Succ (A : Type) `{FGH A} : FGH (sum unit A) := {
   fgh := fun n => iter n (fgh (A := A)) n
+} .
+
+Class FGH_forall {B : nat -> Type} :=
+  fgh_forall : forall n, FGH (B n) .
+
+Instance FGH_sum (B : nat -> Type) `{FGH_forall B} : FGH (sigT B) := {
+  fgh := fun n => fgh (A := B n) (FGH := fgh_forall n) n
 } .
