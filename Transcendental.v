44e760a1ad9041bf0b04c018392887e18b456614
Hexirp <Hexirp@users.noreply.github.com>
2018-12-25 21:20:04 +0900

    Add: 超絶技巧演習問題 - Coq
    参考:
    * https://ja.wikipedia.org/wiki/%E3%83%95%E3%82%A7%E3%83%AB%E3%83%9E%E3%83%BC%E3%81%AE%E6%9C%80%E7%B5%82%E5%AE%9A%E7%90%86
    * https://ja.wikipedia.org/wiki/%E3%82%AB%E3%82%BF%E3%83%A9%E3%83%B3%E4%BA%88%E6%83%B3
    * https://ja.wikipedia.org/wiki/%E3%82%B3%E3%83%A9%E3%83%83%E3%83%84%E3%81%AE%E5%95%8F%E9%A1%8C
    * http://integers.hatenablog.com/entry/2017/09/06/221401

---
+++ Transcendental.v
@@ -0,0 +1,41 @@
+(** * 超絶技巧演習問題 - Coq *)
+
+Require Import Coq.Init.Prelude Coq.Init.Nat.
+
+Open Scope nat_scope.
+
+(** フェルマーの最終定理 *)
+
+Definition felmat
+  : forall n : nat, 2 < n -> ~ exists x y z, x ^ n + y ^ n = z ^ n.
+Proof.
+  (* [Admitted] を [Qed] に取り換えて、この空欄を埋めよ *)
+Admitted.
+
+(** カタラン予想 *)
+
+Definition catalanic a b x y := 1 < a /\ 1 < b /\ 1 < x /\ 1 < y /\ x ^ a = 1 + y ^ b.
+
+Definition catalan
+  : forall a b x y, catalanic a b x y -> x = 3 /\ a = 2 /\ y = 2 /\ b = 3.
+Proof.
+  (* [Admitted] を [Qed] に取り換えて、この空欄を埋めよ *)
+Admitted.
+
+(** バウデットの予想 *)
+Definition baudetic (P : nat -> bool) a b c n := forall p, p < n -> P (a + b * p) = c.
+
+Definition baudet
+  : forall P, exists c, forall n, exists a b, baudetic P a b c n.
+Proof.
+  (* [Admitted] を [Qed] に取り換えて、この空欄を埋めよ *)
+Admitted.
+
+(** コラッツの問題 *)
+Definition collatzic n := if even n then n * 3 + 1 else div2 n.
+
+Definition collatz
+  : forall n, n <> 0 -> exists p, iter p collatzic n = 1.
+Proof.
+  (* [Admitted] を [Qed] に取り換えて、この空欄を埋めよ *)
+Admitted.
