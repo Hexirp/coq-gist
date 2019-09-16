43668ab2c04ce9eedf542dd937d61e83c82d4a5e
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-09-16 18:24:15 +0900

    Update: 値ではなく型を option にする
    値は型と連動しているので、本当に option にする必要があったのは型の
    方だった。

--- ST.v
+++ ST.v
@@ -1,7 +1,15 @@
 Require Import Coq.Init.Prelude Coq.Init.Specif.
 
+(* 自然数をアドレスと考える。アドレス n に型が入っていなければ、値も入っていない。
+   型が入っていれば値も必ず入っている。当然、値が入っていれば型も入っている。 *)
+Definition STMapValue (x : option Type) : Type :=
+    match x with
+    | Some t => t
+    | None => unit
+    end.
+
 (* 関数を Map として見做す。これは STRef が参照する値を保存することが出来る。型レベルでも保存できる。 *)
-Definition STMap : Type := { f : nat -> Type & forall n : nat, option (f n) }.
+Definition STMap : Type := { f : nat -> option Type & forall n : nat, STMapValue (f n) }.
 
 (* s として STMap を取ることを想定している。 *)
 Definition ST (s : Type) (a : Type) : Type := s -> s * a.
