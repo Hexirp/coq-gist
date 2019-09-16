572e3b8451c26c33baa1cbd58647d6393939afd4
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-09-16 18:18:22 +0900

    Update: 値がないことも表せるように

--- ST.v
+++ ST.v
@@ -1,7 +1,7 @@
 Require Import Coq.Init.Prelude Coq.Init.Specif.
 
 (* 関数を Map として見做す。これは STRef が参照する値を保存することが出来る。型レベルでも保存できる。 *)
-Definition STMap : Type := { f : nat -> Type & forall n : nat, f n }.
+Definition STMap : Type := { f : nat -> Type & forall n : nat, option (f n) }.
 
 (* s として STMap を取ることを想定している。 *)
 Definition ST (s : Type) (a : Type) : Type := s -> s * a.
