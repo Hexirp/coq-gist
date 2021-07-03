59f4a331e518126b9f7ba7b88b14a7840f409226
Hexirp <Hexirp@users.noreply.github.com>
2021-07-04 02:13:27 +0900

    Update: Function と Dependent_Function を分ける
    このおかげで TYPE が単純になった。

--- intheo.v
+++ intheo.v
@@ -3,7 +3,16 @@ Module Main.
 Set Universe Polymorphism.
 Set Polymorphic Inductive Cumulativity.
 
-Module Function.
+Module Path.
+
+Inductive T (A : Type) (x : A) : A -> Type
+  :=
+    id : T A x x
+  .
+
+End Path.
+
+Module Dependent_Function.
 
 Inductive T_
   (A : Type)
@@ -92,6 +101,72 @@ Inductive T__
           T__ A A_ A__ B B_ B__ f g x y
   .
 
+End Dependent_Function.
+
+Module Function.
+
+Inductive T_
+  (A : Type)
+  (A_ : A -> A -> Type)
+  (B : Type)
+  (B_ : B -> B -> Type)
+  (f : A -> B)
+  (g : A -> B)
+  : Type
+  :=
+    value_
+      :
+          (forall x : A, forall y : A, A_ x y -> B_ (f x) (g y))
+        ->
+          T_ A A_ B B_ f g
+  .
+
+Inductive T__
+  (A : Type)
+  (A_ : A -> A -> Type)
+  (A__ : forall x : A, forall y : A, A_ x y -> A_ x y -> Type)
+  (B : Type)
+  (B_ : B -> B -> Type)
+  (B__ : forall x : B, forall y : B, B_ x y -> B_ x y -> Type)
+  (f : A -> B)
+  (g : A -> B)
+  (x : T_ A A_ B B_ f g)
+  (y : T_ A A_ B B_ f g)
+  : Type
+  :=
+    value__
+      :
+        forall
+          x_v : forall x : A, forall y : A, A_ x y -> B_ (f x) (g y)
+        ,
+        forall
+          y_v : forall x : A, forall y : A, A_ x y -> B_ (f x) (g y)
+        ,
+          (
+            forall
+              x : A
+            ,
+            forall
+              y : A
+            ,
+            forall
+              p : A_ x y
+            ,
+            forall
+              q : A_ x y
+            ,
+              A__ x y p q
+            ->
+              B__ (f x) (g y) (x_v x y p) (y_v x y q)
+          )
+        ->
+          Path.T (T_ A A_ B B_ f g) x (value_ A A_ B B_ f g x_v)
+        ->
+          Path.T (T_ A A_ B B_ f g) x (value_ A A_ B B_ f g y_v)
+        ->
+          T__ A A_ A__ B B_ B__ f g x y
+  .
+
 Definition compose {A B C : Type} (f : B -> C) (g : A -> B) : A -> C
   := fun x : A => f (g x).
 
@@ -123,16 +198,16 @@ Inductive T_
           Function.T_
             A
             A_
-            (fun x : A => A)
-            (fun x : A => fun y : A => fun p : A_ x y => A_)
+            A
+            A_
             g0
             g1
         ->
           Function.T_
             A
             A_
-            (fun x : A => B)
-            (fun x : A => fun y : A => fun p : A_ x y => B_)
+            B
+            B_
             (Function.compose f g0)
             (Function.compose f g1)
   )
@@ -151,16 +226,16 @@ Inductive T_
           Function.T_
             B
             B_
-            (fun x : B => B)
-            (fun x : B => fun y : B => fun p : B_ x y => B_)
+            B
+            B_
             f0
             f1
         ->
           Function.T_
             A
             A_
-            (fun x : A => B)
-            (fun x : A => fun y : A => fun p : A_ x y => B_)
+            B
+            B_
             (Function.compose f0 g)
             (Function.compose f1 g)
   )
@@ -180,9 +255,10 @@ Inductive T_
               Function.T_
                 B
                 B_
-                (fun x : B => B)
-                (fun x : B => fun y : B => fun p : B_ x y => B_)
-                (Function.compose f g) Function.id
+                B
+                B_
+                (Function.compose f g)
+                Function.id
         ,
         forall
           s
@@ -190,8 +266,8 @@ Inductive T_
               Function.T_
                 A
                 A_
-                (fun x : A => A)
-                (fun x : A => fun y : A => fun p : A_ x y => A_)
+                A
+                A_
                 (Function.compose g f)
                 Function.id
         ,
@@ -199,26 +275,9 @@ Inductive T_
             A
             A_
             A__
-            (fun x : A => B)
-            (fun x : A => fun y : A => fun p : A_ x y => B_)
-            (
-              fun
-                x : A
-              =>
-              fun
-                y : A
-              =>
-              fun
-                p : A_ x y
-              =>
-              fun
-                q : A_ x y
-              =>
-              fun
-                r : A__ x y p q
-              =>
-                B__
-            )
+            B
+            B_
+            B__
             (Function.compose f (Function.compose g f))
             f
             (WR (Function.compose f g) Function.id f r)
@@ -228,15 +287,6 @@ Inductive T_
   .
 
 End TYPE.
-
-Module Path.
-
-Inductive T (A : Type) (x : A) : A -> Type
-  :=
-    id : T A x x
-  .
-
-End Path.
 
 Module Expression.
 
@@ -322,7 +372,7 @@ Inductive T_ (X : Type) (X_ : X -> X -> Type) (x : T X) (y : T X) : Type
         ,
           T_ X X_ x_t y_t
         ->
-          Function.T_
+          Dependent_Function.T_
             X
             X_
             (fun x : X => T X)
@@ -360,7 +410,7 @@ Inductive T_ (X : Type) (X_ : X -> X -> Type) (x : T X) (y : T X) : Type
         ->
           T_ X X_ x_x y_x
         ->
-          Function.T_
+          Dependent_Function.T_
             X
             X_
             (fun x : X => T X)
@@ -398,7 +448,7 @@ Inductive T_ (X : Type) (X_ : X -> X -> Type) (x : T X) (y : T X) : Type
         ,
           T_ X X_ x_t y_t
         ->
-          Function.T_
+          Dependent_Function.T_
             X
             X_
             (fun x : X => T X)
@@ -428,7 +478,7 @@ Inductive T_ (X : Type) (X_ : X -> X -> Type) (x : T X) (y : T X) : Type
         ,
           T_ X X_ x_t y_t
         ->
-          Function.T_
+          Dependent_Function.T_
             X
             X_
             (fun x : X => T X)
